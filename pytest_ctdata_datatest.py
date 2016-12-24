44b81939df81b998823e2bea6c6723a3c09a1ac4
Sasha Cuerda <sasha.cuerda@gmail.com>
2016-12-23 19:18:27 -0500

    aded fixture

--- pytest_ctdata_datatest.py
+++ pytest_ctdata_datatest.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-
+import csv
+from collections import defaultdict
+import yaml
+import os
 
 def pytest_addoption(parser):
     group = parser.getgroup('ctdata_datatest')
@@ -19,3 +22,21 @@ def pytest_addoption(parser):
 @pytest.fixture
 def bar(request):
     return request.config.option.dest_foo
+
+def nested_dict():
+   return defaultdict(nested_dict)
+
+def get_yaml_files():
+    for(dirpath, _, filenames) in os.walk('.'):
+        for filename in filenames:
+            f,e = os.path.splitext(filename)
+            if e == 'yml':
+                yield os.path.join(dirpath, filename)
+
+@pytest.fixture
+def metadata(session):
+    yamls = list(get_yaml_file())
+    with open(yamls[0], 'r') as stream:
+        metadata = yaml.load(stream)
+        return metadata
+
