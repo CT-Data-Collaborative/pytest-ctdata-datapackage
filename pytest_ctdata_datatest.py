1bd92d27c33b232633996bfbf74d3b96ba6c0416
Sasha Cuerda <sasha.cuerda@gmail.com>
2016-12-28 16:57:29 -0500

    added tests

--- pytest_ctdata_datatest.py
+++ pytest_ctdata_datatest.py
@@ -6,22 +6,7 @@ from collections import defaultdict
 import yaml
 import os
 
-def pytest_addoption(parser):
-    group = parser.getgroup('ctdata_datatest')
-    group.addoption(
-        '--foo',
-        action='store',
-        dest='dest_foo',
-        default='2016',
-        help='Set the value for the fixture "bar".'
-    )
-
-    parser.addini('HELLO', 'Dummy pytest.ini setting')
-
-
-@pytest.fixture
-def bar(request):
-    return request.config.option.dest_foo
+CTDATA_DATASET_DOMAINS = ['Civic Vitality', 'Demographics', 'Economy', 'Education', 'Health', 'Housing', 'Safety']
 
 def nested_dict():
    return defaultdict(nested_dict)
@@ -35,8 +20,55 @@ def get_yaml_files():
 
 @pytest.fixture
 def metadata(request):
-    yamlfile = getattr(request.module, "METADATA_FILE")
+    """Load the specified metadata yaml file and parse"""
+    try:
+        yamlfile = getattr(request.module, "METADATA_FILE")
+    except AttributeError:
+        return dict()
     with open(yamlfile, 'r') as stream:
         metadata = yaml.load(stream)
         return metadata
 
+@pytest.fixture
+def expected_years(metadata):
+    """Extract years from metadata and convert to list"""
+    try:
+        return [x for x in metadata['Dataset']['Years in Catalog'][0].split(';')]
+    except KeyError:
+        return []
+
+
+@pytest.fixture
+def spotchecks(metadata):
+    try:
+        [(v[0], v[1], v[2], float(v[3])) for k, v in metadata['Dataset']['Spot Checks'].items()]
+    except KeyError:
+        return []
+
+@pytest.fixture
+def dataset(request):
+    try:
+        datafile = getattr(request.module, 'TARGET_FILE')
+    except AttributeError:
+        return []
+    with open(datafile, 'r') as file:
+        reader = csv.DictReader(file)
+        return [x for x in reader]
+
+
+@pytest.fixture
+def geography(metadata):
+    try:
+        return metadata['Dataset']['Geography']
+    except KeyError:
+        return ''
+
+
+@pytest.fixture
+def geographies(data, geography):
+    return {x[geography] for x in data}
+
+
+@pytest.fixture
+def domain(metadata):
+    return metadata['Dataset']['Domain'] in CTDATA_DATASET_DOMAINS
--- tests/test_ctdata_datatest.py
+++ tests/test_ctdata_datatest.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from pytest_ctdata_datatest import metadata
-
 def test_yaml_parse(testdir):
     """Confirm that pytest can correctly use fixture to load yaml"""
 
@@ -20,6 +18,7 @@ def test_yaml_parse(testdir):
     testdir.makepyfile("""
         METADATA_FILE='test.yml'
 
+
         def test_metadata_fixture(metadata):
             assert metadata['Dataset']['Name'] == 'Children by Family Type'
     """)
@@ -34,64 +33,66 @@ def test_yaml_parse(testdir):
     assert result.ret == 0
 
 
-# def test_bar_fixture(testdir):
-#     """Make sure that pytest accepts our fixture."""
+def test_geography_extraction(testdir):
+    """Confirm that pytest can correctly use fixture to load yaml"""
 
-#     # create a temporary pytest test module
-#     testdir.makepyfile("""
-#         def test_sth(bar):
-#             assert bar == "europython2015"
-#     """)
+    # create a temporary yaml
+    testdir.makefile('.yml', test="""
+        # Children by Family Type
+        ---
+        Dataset:
+            Name: Children by Family Type
+            Description: Children by Family Type reports the number and percent of children living in families by child age and by family type.
+            Geography: Town
+        ...
+    """)
 
-#     # run pytest with the following cmd args
-#     result = testdir.runpytest(
-#         '--foo=europython2015',
-#         '-v'
-#     )
-
-#     # fnmatch_lines does an assertion internally
-#     result.stdout.fnmatch_lines([
-#         '*::test_sth PASSED',
-#     ])
-
-#     # make sure that that we get a '0' exit code for the testsuite
-#     assert result.ret == 0
+    testdir.makepyfile("""
+        METADATA_FILE='test.yml'
 
 
-# def test_help_message(testdir):
-#     result = testdir.runpytest(
-#         '--help',
-#     )
-#     # fnmatch_lines does an assertion internally
-#     result.stdout.fnmatch_lines([
-#         'ctdata_datatest:',
-#         '*--foo=DEST_FOO*Set the value for the fixture "bar".',
-#     ])
+        def test_metadata_geography(geography):
+            assert geography == 'Town'
+    """)
+
+    result = testdir.runpytest(
+        '-v'
+    )
+    result.stdout.fnmatch_lines([
+        '*::test_metadata_geography PASSED',
+    ])
+
+    assert result.ret == 0
+
+def test_metadata_domain_extract(testdir):
+    """Confirm that pytest can correctly use fixture to load yaml"""
+
+    # create a temporary yaml
+    testdir.makefile('.yml', test="""
+        # Children by Family Type
+        ---
+        Dataset:
+            Name: Children by Family Type
+            Description: Children by Family Type reports the number and percent of children living in families by child age and by family type.
+            Geography: Town
+            Domain: Demographics
+        ...
+    """)
+
+    testdir.makepyfile("""
+        METADATA_FILE='test.yml'
 
 
-# def test_hello_ini_setting(testdir):
-#     testdir.makeini("""
-#         [pytest]
-#         HELLO = world
-#     """)
+        def test_metadata_domain(domain):
+            assert domain
+    """)
 
-#     testdir.makepyfile("""
-#         import pytest
+    result = testdir.runpytest(
+        '-v'
+    )
+    result.stdout.fnmatch_lines([
+        '*::test_metadata_domain PASSED',
+    ])
 
-#         @pytest.fixture
-#         def hello(request):
-#             return request.config.getini('HELLO')
+    assert result.ret == 0
 
-#         def test_hello_world(hello):
-#             assert hello == 'world'
-#     """)
-
-#     result = testdir.runpytest('-v')
-
-#     # fnmatch_lines does an assertion internally
-#     result.stdout.fnmatch_lines([
-#         '*::test_hello_world PASSED',
-#     ])
-
-#     # make sure that that we get a '0' exit code for the testsuite
-#     assert result.ret == 0
